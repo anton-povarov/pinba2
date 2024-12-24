ec9e7f2addeeaf2d335ad07c4d23ad4c2d08f67d
Anton Povarov <anton.povarov@gmail.com>
2024-12-24 21:25:30 +0400

    readme and docs updates

--- docker/build-dependencies.sh
+++ docker/build-dependencies.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -xe
 
 # mariadb
+# the annoying part - why we're building mariadb from source at all, 
+#  is that we need internal headers that are not present in regular apt packages
+#  so the idea here is to avoid building the whole of mariadb, but only the parts we need
+#  we only need to run initial generation stage with cmake . and build a few libs quickly
+# the final tricky part is to build with the same compiler flags as the server itself
+#  for now - there is no problem, unless mariadb is built with debug, in which case pinba should be configured with --enable-debug
 cd /_src/mariadb
 cmake .
-
-# don't want to build the whole thing, it takes a long time, just what we need
 make -C libservices
 # boost tries to include this file globally and includes this one; but mariadb needs this to install
 # mv+rm is a workaround for docker build-ing on macos case-insensitive fs
